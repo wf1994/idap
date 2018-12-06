c287009dc1354f01c8d046254b157d05212b2299
wf1994 <weifeng6000@163.com>
2018-12-06 20:38:02 +0800

    java

---
+++ src/main/java/com/demo/idap/controller/helloController.java
@@ -0,0 +1,15 @@
+
+package com.demo.idap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = {"/hello"})
+public class helloController {
+
+    @RequestMapping(value = {"/springboot"})
+    public String hello(){
+        return "HelloWord";
+    }
+}
---
+++ src/main/java/com/demo/idap/service/GetCloudDataService.java
@@ -0,0 +1,45 @@
+package com.demo.idap.service;
+
+import com.demo.idap.entity.CloudAllData;
+import com.demo.idap.entity.CloudData;
+import com.demo.idap.entity.Cloudsort;
+import com.demo.idap.mapper.CloudDataMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Service
+public class GetCloudDataService {
+    public CloudDataMapper getCloudatamapper() {
+        return cloudatamapper;
+    }
+
+    /**
+     * 注入dao
+     */
+    @Autowired
+    private CloudDataMapper cloudatamapper;
+
+    //根据level查询出每个云
+    public List<Cloudsort> getCloudSort(int level){
+        return cloudatamapper.getCloudSort(level);
+    }
+
+    //根据cloudcode取出指标+中文与单位名
+    public  List<CloudData>  getIndexChinese(int cloudcode){
+        return cloudatamapper.getIndexChinese(cloudcode);
+    }
+    //根据cloudcode取出指标+数据
+    public List<CloudAllData> getIndexData(int cloudcode){
+        return cloudatamapper.getIndexData(cloudcode);
+    }
+
+    //查询三年（2016、2017、2018）的用户量数据-
+    public List<CloudAllData> getYearsData(){
+        return cloudatamapper.getYearsData();
+    }
+
+}

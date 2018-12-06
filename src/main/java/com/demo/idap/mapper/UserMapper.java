3cd1b195c45299e3678ab66d1d866a32501d6479
wf1994 <weifeng6000@163.com>
2018-12-06 11:41:41 +0800

    cehsi

--- src/main/java/com/demo/idap/mapper/UserMapper.java
+++ src/main/java/com/demo/idap/mapper/UserMapper.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
 
 @Mapper
 @Component

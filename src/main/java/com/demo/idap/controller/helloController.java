d202d3e0de090ff165b037108dc289950fca3153
tanghaonan <471327099@qq.com>
2018-12-07 10:28:09 +0800

    88

--- src/main/java/com/demo/idap/controller/helloController.java
+++ src/main/java/com/demo/idap/controller/helloController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class helloController {
 
 
-
     @RequestMapping(value = {"/springboot"})
     public String hello(){
         return "HelloWord";
